3309d06e94b590e5577449f74e8ece6b2a906b4e
MatiasOso <114182943+MatiasOso@users.noreply.github.com>
2023-09-20 21:48:08 -0300

    Error on localhost:8000/task(s?)/id fix it

--- myapp/urls.py
+++ myapp/urls.py
@@ -3,8 +3,12 @@ from . import views
 
 
 urlpatterns = [
-    path('',views.hello),
-    path('about/',views.about)        
+    path('',views.index),
+    path('about/',views.about),
+    path('hello/<str:username>',views.hello),
+    path('projects/',views.projects),
+    path('tasks/<int:id>',views.tasks)
+            
 ]
 
 # django.core.exceptions.ImproperlyConfigured: The included URLconf '<module 'myapp.urls' from 'C:\\Users\\matio\\OneDrive\\Documentos\\djangoproject\\DjangoProject\\myapp\\urls.py'>' 
--- myapp/views.py
+++ myapp/views.py
@@ -1,10 +1,26 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse,JsonResponse
+from .models import Project,Task
+
+def index(request):
+    return HttpResponse("Index Page")
 
 # Create your views here.
-def hello(request):
-    return HttpResponse("<h1>Hello world</h1>")
+def hello(request,username):
+    return HttpResponse("<h1>Hello  %s</h1>" % username)
 
+# def hello(request,id):
+#     # print(type(id))
+#     result = id + 100 * 2
+#     return HttpResponse("<h1>Hello %s</h1>" % result)
+   
+def projects(request):
+    projects = list(Project.objects.values())
+    return JsonResponse(projects,safe=False)
+
+def tasks(request,id):
+    task = Task.objects.get(id=id) #(pk)
+    return HttpResponse('task: %s ' % tasks.title)
 
 def about(request):
     return HttpResponse("About")
