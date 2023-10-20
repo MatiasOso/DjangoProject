2f4c18ceb6739f41b37de47a34aefc6edfaa6051
MatiasOso <114182943+MatiasOso@users.noreply.github.com>
2023-10-20 18:03:57 -0300

    salaverga tutorail terminado gg izi nubs

--- myapp/views.py
+++ myapp/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render,redirect
 from django.http import HttpResponse,JsonResponse
 from .models import Project,Task
 from django.shortcuts import get_object_or_404,render
-from .forms import CreateNewTask
+from .forms import CreateNewTask,CreateNewProject
 
 def index(request):
     # a futuro hare esto con una base de datos real (quizas db4free)
@@ -31,7 +31,7 @@ def hello(request,username):
 def projects(request):
     # projects = list(Project.objects.values())
     projects = Project.objects.all()
-    return render(request,'projects.html',{
+    return render(request,'projects/projects.html',{
         'projects': projects
     })
 
@@ -40,17 +40,35 @@ def tasks(request): #(request,id)
     # tasks = Task.objects.get(title=title) 
     # tasks = get_object_or_404(Task, title = title) # I NEED TO FIX THIS
     tasks =  Task.objects.all()
-    return render(request,'tasks.html',{
+    return render(request,'tasks/tasks.html',{
         'tasks' : tasks 
     })
     
 def create_task(request):
     if request.method == 'GET':
         # show interface
-        return render(request,'create_task.html',{
+        return render(request,'tasks/create_task.html',{
             'form':CreateNewTask()
         })
     else:
         Task.objects.create(title=request.POST['title'],
                             description=request.POST['description'],project_id=2)
-        return redirect('/tasks/')
+        return redirect('tasks')
+    
+    
+def create_project(request):
+    if request.method == 'GET':
+        return render(request,'projects/create_project.html',{
+            'form':CreateNewProject
+        })
+    else:
+        Project.objects.create(name=request.POST['name'])
+        return redirect('projects')
+
+def project_detail(request, id):
+    project = get_object_or_404(Project,id=id)
+    tasks = Task.objects.filter(project_id = id)
+    return render(request, 'projects/detail.html',{
+        'project': project,
+        'tasks': tasks
+    })
